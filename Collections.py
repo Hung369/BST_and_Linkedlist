874865607d71e13879fa9ecd09fa8bef1b177128
Hung369 <nguyenmhung2310@gmail.com>
2023-08-26 15:11:08 +0700

    first commit

---
+++ Collections.py
@@ -0,0 +1,135 @@
+import math
+
+class Node:  # init node class
+    def __init__(self, dataval):
+        self.value = dataval
+
+    def getVal(self):
+        return self.value
+
+
+class ListNode(Node):  # node class for linked list
+    def __init__(self, dataval):
+        super().__init__(dataval)
+        self.next = None
+
+
+class LinkedList:  # singly linked list class
+    def __init__(self):
+        self.head = None
+
+    def AtEnd(self, newdata):
+        NewNode = ListNode(newdata)
+        if self.head is None:
+            self.head = NewNode
+            return
+        last = self.head
+        while (last.next):
+            last = last.next
+        last.next = NewNode
+
+    def listprint(self):
+        printval = self.head
+        while printval is not None:
+            print(printval.getVal())
+            printval = printval.next
+
+
+class TreeNode(Node):  # Binary Tree node
+    def __init__(self, dataval):
+        super().__init__(dataval)
+        self.left = None
+        self.right = None
+
+
+class BST:  # Binary Search Tree class
+    def __init__(self):
+        self.root = None
+
+    def Append(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
+            return
+        else:
+            self.insert(value, self.root)
+
+    def insert(self, data, current_node):
+        if data < current_node.getVal():
+            if current_node.left is None:
+                current_node.left = TreeNode(data)
+            else:
+                self.insert(data, current_node.left)
+        elif data > current_node.getVal():
+            if current_node.right is None:
+                current_node.right = TreeNode(data)
+            else:
+                self.insert(data, current_node.right)
+        else:
+            print("Value already in tree")
+
+    def FirstOrder(self):
+        if self.root is not None:
+            self.first_order_traversal(self.root)
+
+    def first_order_traversal(self, current_node):
+        if current_node is not None:
+            print(current_node.getVal())
+            self.first_order_traversal(current_node.left)
+            self.first_order_traversal(current_node.right)
+
+    def MaxNode(self): # traverse the node from root to right recursively until the right is NULL.
+        if self.root is None:
+            print("Root isn't available")
+        else:
+            pointer = self.root
+            while pointer.right is not None:
+                pointer = pointer.right
+            return pointer.getVal()
+
+    def MinNode(self): # traverse the node from root to left recursively until the left is NULL.
+        if self.root is None:
+            print("Root isn't available")
+        else:
+            pointer = self.root
+            while pointer.left is not None:
+                pointer = pointer.left
+            return pointer.getVal()
+
+    def SearchValue(self, value):
+        if self.root is None:
+            print("Root isn't available")
+        else:
+            pointer = self.root
+            boolean = self.finding_node(pointer, value = value)
+            return boolean
+
+    def finding_node(self, pointer, value): # binary search
+        if not pointer:
+            return False
+        if pointer.getVal() == value:
+            return True
+        if pointer.getVal() > value:
+            return self.finding_node(pointer.left, value)
+        return self.finding_node(pointer.right, value)
+    
+    def Height(self):
+        if self.root is None:
+            return 0
+        else:
+            pointer = self.root
+            height = self.Depth(pointer)
+            return height
+    
+    def Depth(self, node):
+        if node is None: return 0
+        leftDepth = self.Depth(node.left)
+        rigthDepth = self.Depth(node.right)
+        return max(leftDepth, rigthDepth) + 1
+    
+    def TreeVal(self):
+        if self.root is None:
+            return 0
+        return self.root.getVal() + self.TreeVal(self.root.left) + self.TreeVal(self.root.right)
+
+    def Sum_All_Nodes_on_K_level(self, k):
+        pass
---
+++ main.py
@@ -0,0 +1,34 @@
+from Collections import *
+
+
+def CreateList():
+    head = LinkedList()
+    num = int(input('Nhập số lượng node: '))
+    for i in range(num):
+        value = int(input(f'Nhập giá trị thứ {i+1}: '))
+        head.AtEnd(value)
+
+    head.listprint()
+    return head
+
+
+def BinaryTree():
+    root = BST()
+    num = int(input('Nhập số lượng node: '))
+    for i in range(num):
+        value = int(input(f'Nhập giá trị thứ {i+1}: '))
+        root.Append(value)
+
+    root.FirstOrder()
+    return root
+
+
+if __name__ == "__main__":
+    root = BinaryTree()
+    num = 1
+    print("\n------------------------\n")
+    if root.SearchValue(num):
+        print(f"{num} is available in the Binary Search Tree")
+    print(f"The maximum value in BST is {root.MaxNode()}")
+    print(f"The minimum value in BST is {root.MinNode()}")
+    print(f"BST height is {root.Height()}")
