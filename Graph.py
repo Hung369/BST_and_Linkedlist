b2d82a42688f07c28eab5cc21f4e1051dd6cdb83
Hung369 <nguyenmhung2310@gmail.com>
2023-08-31 08:44:27 +0700

    Add DiGraph class

--- Graph.py
+++ Graph.py
@@ -92,3 +92,25 @@ class Undirected_Graph:
                     return True
 
         return False
+
+
+class Directed_Graph:
+    def __init__(self, number):
+        self.__V = number
+        self.__adj = {}
+        for vertex in range(self.__V):
+            self.__adj[vertex] = set()
+
+    def addEdge(self, v, w):
+        if v not in self.__adj and w not in self.__adj:
+            self.__V += 1
+        self.__adj[v].add(w)
+
+    def adjacent(self, v):
+        return self.__adj[v]
+
+    def num_Of_Vertex(self):
+        return self.__V
+
+    def show_graph(self):
+        return self.__adj
